7f1d7ccc2a8a753bd0e15352c13eeb34f7f5ebdd
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2023-01-17 17:26:09 +0100

    first page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/chi-sono', function () {
+    return view('chisono');
+    echo "ciao";
+})->name('chi-sono');
